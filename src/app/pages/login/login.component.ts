02bfc1b07337dcd4c542e7dc99ffee5cc91998e9
Nikolai Amosov <nikolai_sergeevich@mail.ru>
2024-12-12 17:11:14 +0300

    login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    RouterLink,
+    MatSnackBarModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  
+  matSnackBar = inject(MatSnackBar);
+  fb = inject(FormBuilder);
+  router = inject(Router);
+  hide = true;
+  form!: FormGroup;
 
+  ngOnInit(): void {
+    this.initializeLoginForm();
+  }
+
+  private initializeLoginForm(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    }); 
+  }
 }
